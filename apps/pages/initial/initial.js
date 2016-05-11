2a8b5b8223bd1e3fe8b229392ee56a59f605f9b8
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-05-12 04:11:58 +0800

    agreement

--- apps/pages/initial/initial.js
+++ apps/pages/initial/initial.js
@@ -6,9 +6,25 @@ mobileMJ.config(["$stateProvider", function($stateProvider){
   });
 }])
 
-.controller("initialCtrl", ["$scope", "$state", function($scope, $state){
+.controller("initialCtrl", ["$scope", "$state", "$uibModal", function($scope, $state, $uibModal){
   $scope.submit = function(){
     $state.go("dashboard");
   };
+
+  $scope.agree = false;
+
+  $scope.durations = [{"value": "一个月", "name": "一个月"},{"value": "二个月", "name": "二个月"},{"value": "三个月", "name": "三个月"}];
+
+  $scope.selectedDuration = $scope.durations[0].value;
+
+  $scope.showDetail = function(){
+    $uibModal.open({
+      templateUrl: "/pages/initial/agreement.html",
+      controller: "agreementCtrl"
+    });
+  };
+  
+}])
+.controller("agreementCtrl", ["$scope", function($scope){
   
 }]);
