76e6b002cf30681d32894b6a58dfbae426ef76ce
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-05-11 21:42:41 +0800

    update dashboard

--- apps/pages/dashboard/dashboard.js
+++ apps/pages/dashboard/dashboard.js
@@ -10,16 +10,17 @@ mobileMJ.config(["$stateProvider", function($stateProvider){
   $scope.status = 0; //none created applied validated
   $scope.message = "当前没有进行中的贷款申请,您可以提交一份贷款意向进行申请.";
 
+  var messageCollection = {
+    0: "当前没有进行中的贷款申请,您可以提交一份贷款意向进行申请.",
+    1: "您的基本信息已通过审核,请正式发起贷款申请.",
+    2: "您的贷款申请已提交,请耐心等待审核.",
+    3: ""
+  };
 
   //todo test only
   $scope.changeStatus = function(){
-    $scope.status = $scope.status + 1;
-    $scope.message = {
-      0: "当前没有进行中的贷款申请,您可以提交一份贷款意向进行申请.",
-      1: "您的基本信息已通过审核,请正式发起贷款申请.",
-      2: "您的贷款申请已提交,请耐心等待审核.",
-      3: ""
-    }($scope.status);
+    $scope.status = ($scope.status + 1)%4;
+    $scope.message = messageCollection[$scope.status];
   };
 
 }]);
