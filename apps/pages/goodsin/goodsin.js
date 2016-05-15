6d05fad797d99284995fa6d2fc8e4b25da3b7950
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-05-15 21:30:00 +0800

    goods in pages

---
+++ apps/pages/goodsin/goodsin.js
@@ -0,0 +1,40 @@
+mobileMJ.config(["$stateProvider", function($stateProvider){
+  $stateProvider.state("goodsin", {
+    url: "/goodsin",
+    templateUrl: "/pages/goodsin/goodsin.html",
+    controller: "goodsinCtrl"
+  });
+}])
+  
+.controller("goodsinCtrl", ["$scope", "$state", "$uibModal", function($scope, $state, $uibModal){
+  $scope.submit = function(){
+    $state.go("dashboard");
+  };
+  
+  $scope.addGoods = function(){
+    $uibModal.open({
+      templateUrl: "/pages/goodsin/add.html",
+      controller: "goodsinAddCtrl",
+      windowClass: "mj-page-goodsin-add-window",
+      backdrop: "static",
+      keyboard: false
+    }).result.then(function(data){
+      //todo
+    },function(){});
+  };
+  
+}])
+
+.controller("goodsinAddCtrl", ["$scope", "$uibModalInstance", function($scope, $uibModalInstance){
+  
+  
+  $scope.submit = function(){
+    $uibModalInstance.close($scope.imageURI);
+  };
+  
+  $scope.cancel = function(){
+    $uibModalInstance.dismiss();
+  };
+  
+  
+}]);
