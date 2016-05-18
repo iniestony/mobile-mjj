9617afcea37d9a6684d5298470a9b1e4880d534e
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-05-19 02:43:24 +0800

    update

--- apps/pages/moneyin/moneyin.js
+++ apps/pages/moneyin/moneyin.js
@@ -6,14 +6,13 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
   });
 }])
   
-.controller("moneyinCtrl", ["$scope", "$state", "$uibModal", function($scope, $state, $uibModal){
-  $scope.submit = function(){
-    $state.go("dashboard");
-  };
-  
+.controller("moneyinCtrl", ["$scope", "$state", "$http", "xhrRequestOrigin", function($scope, $state, $http, xhrRequestOrigin){
+
+  $scope.details = [];
   $scope.date = new Date();
   $scope.opened = false;
-
+  $scope.repayAmount = 0;
+  
   $scope.dateOptions = {
     formatYear: 'yy',
     maxDate: new Date(2020, 5, 27),
@@ -21,20 +20,51 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
     startingDay: 1
   };
 
+  $http.get(xhrRequestOrigin + "/api/loandetails/data.do?action=list_loan&loanid=238").success(function(data){
+    for(var i=0;i<data.value.length;i++){
+      var obj = {
+        "idloandetail": data.value[i].idloandetail,
+        "amount": parseFloat(parseFloat(data.value[i].amount).toFixed(2)),
+        "repaid": parseFloat(parseFloat(data.value[i].repaid).toFixed(2)),
+        "remain": parseFloat(parseFloat(data.value[i].amount).toFixed(2)) - parseFloat(parseFloat(data.value[i].repaid).toFixed(2)),
+        "usedate": getDate(data.value[i].usedate),
+        "repaymentdate": getDate(data.value[i].repaymentdate),
+        "interest": parseFloat(data.value[i].interest),
+        "repaying": parseFloat(parseFloat(data.value[i].repaying).toFixed(2))
+      };
+      obj.current = parseFloat((obj.remain + (obj.remain * obj.interest * (daysInterval(new Date(obj.usedate), new Date) + 1) / 360)).toFixed(2));
+      obj.expire = parseFloat((obj.remain + (obj.remain * obj.interest * (daysInterval(new Date(obj.usedate), new Date(obj.repaymentdate)) + 1) / 360)).toFixed(2));
+      $scope.details.push(obj);
+    }
+    $scope.date = new Date($scope.details[0].usedate);
+    $scope.dateOptions.minDate = new Date($scope.details[0].usedate);
+    $scope.dateOptions.maxDate = new Date($scope.details[0].repaymentdate);
+    $scope.current = angular.copy($scope.details[0]);
+  });
+
+  function getDate(str){
+    var d = new Date(str);
+    return d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+  }
+
+  function daysInterval(d1, d2){
+    return parseInt((d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
+  }
+
   $scope.toggleDatePicker = function(){
     $scope.opened = !$scope.opened;
   };
+
+  $scope.selectDetail = function(detail){
+    $scope.date = new Date(detail.usedate);
+    $scope.dateOptions.minDate = new Date(detail.usedate);
+    $scope.dateOptions.maxDate = new Date(detail.repaymentdate);
+    $scope.current = angular.copy(detail);
+  };
   
-  // $scope.addGoods = function(){
-  //   $uibModal.open({
-  //     templateUrl: "/pages/moneyout/add.html",
-  //     controller: "goodsoutAddCtrl",
-  //     windowClass: "sjd-page-goodsout-add-window",
-  //     backdrop: "static",
-  //     keyboard: false
-  //   }).result.then(function(data){
-  //     //todo
-  //   },function(){});
-  // };
+  
+  $scope.submit = function(){
+    $state.go("dashboard");
+  };
   
 }]);
