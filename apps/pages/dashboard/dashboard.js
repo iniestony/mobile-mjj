a76955a8c9ca3a95d2ad4bb131a8b6dd78af2625
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-05-19 00:17:34 +0800

    get dashboard status 3

--- apps/pages/dashboard/dashboard.js
+++ apps/pages/dashboard/dashboard.js
@@ -6,7 +6,9 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
   });
 }])
 
-.controller("dashboardCtrl", ["$scope", "$state", "$stateParams", "$uibModal", function($scope, $state, $stateParams, $uibModal){
+.controller("dashboardCtrl", ["$scope", "$state", "$stateParams", "$uibModal", "xhrRequestOrigin", "$http",
+  function($scope, $state, $stateParams, $uibModal, xhrRequestOrigin, $http){
+
   $scope.status = 0; //none created applied validated
   $scope.message = "当前没有进行中的贷款申请,您可以提交一份贷款意向进行申请.";
 
@@ -17,10 +19,32 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
     3: ""
   };
 
+  var callbackCollection = {
+    0: function(){},
+    1: function(){},
+    2: function(){},
+    3: function(){
+      $http.get(xhrRequestOrigin + "/api//loanstatement/data.do?loanid=152&action=loan").success(function(data){
+        $scope.usedamount = data.value.usedamount;
+        $scope.ratio = data.value.ratio;
+        $scope.amount = parseFloat((data.value.amount /10000).toFixed(2));
+        $scope.expireddate = data.value.expireddate;
+        $scope.minimum = parseFloat((parseFloat($scope.usedamount) / parseFloat($scope.ratio)).toFixed(0));
+
+        $http.get(xhrRequestOrigin + "/bankvisit//loan/stockValue.do?loanid=238").success(function(data1){
+          $scope.core = parseFloat(data1.totalvalue.toFixed(0));
+          $scope.out = $scope.core - $scope.minimum;
+        });
+
+      });
+    }
+  };
+
   //todo test only
   $scope.changeStatus = function(){
     $scope.status = ($scope.status + 1)%4;
     $scope.message = messageCollection[$scope.status];
+    callbackCollection[$scope.status]();
   };
 
   
