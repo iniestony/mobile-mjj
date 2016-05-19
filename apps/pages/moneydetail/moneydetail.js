377318e67b4199026857101b3f0102d699ff3543
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-05-19 16:50:08 +0800

    fix fresh status

--- apps/pages/moneydetail/moneydetail.js
+++ apps/pages/moneydetail/moneydetail.js
@@ -11,23 +11,28 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
 
   $scope.details = [];
 
-  $http.get(xhrRequestOrigin + "/api/loandetails/data.do?action=list_loan&loanid=238").success(function(data){
-    for(var i=0;i<data.value.length;i++){
-      var obj = {
-        "idloandetail": data.value[i].idloandetail,
-        "amount": parseFloat(parseFloat(data.value[i].amount).toFixed(2)),
-        "repaid": parseFloat(parseFloat(data.value[i].repaid).toFixed(2)),
-        "remain": parseFloat(parseFloat(data.value[i].amount).toFixed(2)) - parseFloat(parseFloat(data.value[i].repaid).toFixed(2)),
-        "usedate": getDate(data.value[i].usedate),
-        "repaymentdate": getDate(data.value[i].repaymentdate),
-        "interest": parseFloat(data.value[i].interest),
-        "repaying": parseFloat(parseFloat(data.value[i].repaying).toFixed(2))
-      };
-      obj.current = parseFloat((obj.remain + (obj.remain * obj.interest * (daysInterval(new Date(obj.usedate), new Date()) + 1) / 360)).toFixed(2));
-      obj.expire = parseFloat((obj.remain + (obj.remain * obj.interest * (daysInterval(new Date(obj.usedate), new Date(obj.repaymentdate)) + 1) / 360)).toFixed(2));
-      $scope.details.push(obj);
-    }
-  });
+  function refresh(){
+    $http.get(xhrRequestOrigin + "/api/loandetails/data.do?action=list_loan&loanid=238").success(function(data){
+      $scope.details = [];
+      for(var i=0;i<data.value.length;i++){
+        var obj = {
+          "idloandetail": data.value[i].idloandetail,
+          "amount": parseFloat(parseFloat(data.value[i].amount).toFixed(2)),
+          "repaid": parseFloat(parseFloat(data.value[i].repaid).toFixed(2)),
+          "remain": parseFloat(parseFloat(data.value[i].amount).toFixed(2)) - parseFloat(parseFloat(data.value[i].repaid).toFixed(2)),
+          "usedate": getDate(data.value[i].usedate),
+          "repaymentdate": getDate(data.value[i].repaymentdate),
+          "interest": parseFloat(data.value[i].interest),
+          "repaying": parseFloat(parseFloat(data.value[i].repaying).toFixed(2))
+        };
+        obj.current = parseFloat((obj.remain + (obj.remain * obj.interest * (daysInterval(new Date(obj.usedate), new Date()) + 1) / 360)).toFixed(2));
+        obj.expire = parseFloat((obj.remain + (obj.remain * obj.interest * (daysInterval(new Date(obj.usedate), new Date(obj.repaymentdate)) + 1) / 360)).toFixed(2));
+        $scope.details.push(obj);
+      }
+    });
+  }
+
+  refresh();
 
   $scope.repay = function(detail){
     $uibModal.open({
@@ -41,7 +46,9 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
           return angular.copy(detail);
         }
       }
-    }).result.then(function(){},function(){});
+    }).result.then(function(){
+      refresh();
+    },function(){});
   };
   
   $scope.apply = function(){
