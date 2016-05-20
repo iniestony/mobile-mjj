b5e5d52bb5b6662ac6d2164b15f57fdd7a52319f
iniestony <iniestony@iniestony-Macbook-Pro.local>
2016-05-21 01:48:07 +0800

    updatye

--- apps/pages/goodsout/goodsout.js
+++ apps/pages/goodsout/goodsout.js
@@ -11,6 +11,8 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
     $state.go("dashboard");
   };
 
+  $scope.products = [];
+
   $scope.addGoods = function(){
     $uibModal.open({
       templateUrl: "/pages/goodsout/add.html",
@@ -19,17 +21,62 @@ mobileSJD.config(["$stateProvider", function($stateProvider){
       backdrop: "static",
       keyboard: false
     }).result.then(function(data){
-      //todo
+      var existingIDs = $scope.products.map(function(p){
+        return p.erpid;
+      });
+      for(var i=0;i<data.length;i++){
+        if(existingIDs.indexOf(data[i].erpid) < 0){
+          $scope.products.push({
+            "erpid": data[i].erpid,
+            "name": data[i].name,
+            "count": 0,
+            "mortgaged": data[i].mortgaged,
+            "dealprice": data[i].dealprice,
+            "totalquantity": data[i].totalquantity
+          });
+        }
+      }
     },function(){});
   };
   
 }])
 
-.controller("goodsoutAddCtrl", ["$scope", "$uibModalInstance", function($scope, $uibModalInstance){
+.controller("goodsoutAddCtrl", ["$scope", "$uibModalInstance", "$http", "xhrRequestOrigin", "sjdDialog",
+  function($scope, $uibModalInstance, $http, xhrRequestOrigin, sjdDialog){
+
+  $scope.productName = "";
+  $scope.products = [];
+  
+  $scope.search = function(){
+    var url = xhrRequestOrigin + "/stockproducts/customerproducts/list.do?enterpriseid=240";
+    if($scope.productName !== ""){
+      url = url + "&productname=" + $scope.productName;
+    }
+
+    $http.get(url).success(function(data){
+      $scope.products = eval("(" + data.value + ")");
+      for(var i=0;i<$scope.products.length;i++){
+        $scope.products[i].selected = false;
+      }
+    }).error(function(msg){
+      sjdDialog.open("Error", msg);
+    });
+  };
   
 
   $scope.submit = function(){
-    $uibModalInstance.close($scope.imageURI);
+    var result = $scope.products.filter(function(item){
+      return item.selected;
+    }).map(function(n_item){
+      return {
+        "erpid": n_item.erpid,
+        "name": n_item.name,
+        "mortgaged": n_item.mortgaged,
+        "dealprice": n_item.dealprice,
+        "totalquantity": n_item.totalquantity
+      };
+    });
+    $uibModalInstance.close(result);
   };
 
   $scope.cancel = function(){
