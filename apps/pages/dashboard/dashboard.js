5d7f29f871c4a79048c9264593f35e23341ead56
jinjieMao <jnavie2013@gmail.com>
2016-05-31 11:44:28 +0800

    修改首页save

--- apps/pages/dashboard/dashboard.js
+++ apps/pages/dashboard/dashboard.js
@@ -23,22 +23,53 @@ mobileSJD.config(["$stateProvider", function($stateProvider) {
                 $rootScope.project.status = response.status;
                 $rootScope.project.loanid = response.loanid;
                 $scope.status = response.status;
+                $scope.message = messageCollection[$scope.status];
+                callbackCollection[$scope.status]();
             });
 
 
             var messageCollection = {
-                0: "当前没有进行中的贷款申请,您可以提交一份贷款意向进行申请.",
-                1: "您的基本信息已通过审核,请正式发起贷款申请.",
-                2: "您的贷款申请已提交,请耐心等待审核.",
-                3: ""
+                1: "请填写初步调查问卷并提交",
+                2: "您的基本情况已提交，请等待工作人员的核实",
+                18: "您的贷款申请已提交,请耐心等待审核",
+                12: "您的贷款申请已提交,请耐心等待审核",
+                19: "您的贷款申请已提交,请耐心等待审核",
+                16: "您的贷款申请已提交,请耐心等待审核",
+                181: "很抱歉，我们暂时无法为您提供服务",
+                20: "恭喜您！您的贷款意向已通过数据贷评估，请发起正式申请",
+                21: "您正在进行贷款申请，请填写完整的申请表并提交",
+                22: "您的贷款资料审核不通过，请补充、修改贷款资料",
+                262: "非常抱歉, 您的贷款申请被拒绝",
+                8: "非常抱歉, 您的贷款申请被拒绝",
+                23: "您的贷款申请已提交资金方审批，请耐心等候",
+                260: "您的贷款申请已提交资金方审批，请耐心等候",
+                26: "您的贷款申请已提交资金方审批，请耐心等候",
+                27: "恭喜您！您的贷款已通过资金方审批",
+                5: "",
+                35: "",
+                79: ""
+
             };
 
             var callbackCollection = {
-                0: function() {},
                 1: function() {},
                 2: function() {},
-                3: function() {
-                    $http.get(xhrRequestOrigin + "/api//loanstatement/data.do?action=loan&loanid=" + $rootScope.project.id).success(function(data) {
+                18: function() {},
+                12: function() {},
+                19: function() {},
+                16: function() {},
+                181: function() {},
+                20: function() {},
+                21: function() {},
+                22: function() {},
+                262: function() {},
+                8: function() {},
+                23: function() {},
+                260: function() {},
+                26: function() {},
+                27: function() {},
+                5: function() {
+                    $http.get(xhrRequestOrigin + "/api//loanstatement/data.do?action=loan&loanid=" + $rootScope.project.loanid).success(function(data) {
                         $scope.usedamount = data.value.usedamount;
                         $scope.ratio = data.value.ratio;
                         $scope.amount = parseFloat((data.value.amount / 10000).toFixed(2));
@@ -46,21 +77,23 @@ mobileSJD.config(["$stateProvider", function($stateProvider) {
                         $scope.expireddate = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
                         $scope.minimum = parseFloat((parseFloat($scope.usedamount) / parseFloat($scope.ratio)).toFixed(0));
 
-                        $http.get(xhrRequestOrigin + "/bankvisit//loan/stockValue.do?loanid=" + $rootScope.project.id).success(function(data1) {
+                        $http.get(xhrRequestOrigin + "/bankvisit//loan/stockValue.do?loanid=" + $rootScope.project.loanid).success(function(data1) {
                             $scope.core = parseFloat(data1.totalvalue.toFixed(0));
                             $scope.out = $scope.core - $scope.minimum;
                         });
 
                     });
-                }
+                },
+                35: function() {},
+                79: function() {},
             };
 
-            //todo test only
-            $scope.changeStatus = function() {
-                $scope.status = ($scope.status + 1) % 4;
-                $scope.message = messageCollection[$scope.status];
-                callbackCollection[$scope.status]();
-            };
+            // //todo test only
+            // $scope.changeStatus = function() {
+            //     $scope.status = ($scope.status + 1) % 4;
+            //    
+            //     callbackCollection[$scope.status]();
+            // };
 
 
 
